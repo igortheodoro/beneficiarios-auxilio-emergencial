aef8a3741df1b482de4529cd658decebf5d11ab4
Igor Theodoro <igortheodoro12@gmail.com>
2020-07-24 19:56:51 -0300

    Updates

---
+++ precache-manifest.c5be41a0bd3bfafec231ed94421bf41e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9231c4e7dee3b41cab893ce21c4e0e2",
+    "url": "/beneficiados-auxilio-emergencial/index.html"
+  },
+  {
+    "revision": "b7611d20cd3d636f55d8",
+    "url": "/beneficiados-auxilio-emergencial/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f306a16a8731bda0e2f5",
+    "url": "/beneficiados-auxilio-emergencial/static/js/2.98f7d3be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beneficiados-auxilio-emergencial/static/js/2.98f7d3be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7611d20cd3d636f55d8",
+    "url": "/beneficiados-auxilio-emergencial/static/js/main.729ef7c9.chunk.js"
+  },
+  {
+    "revision": "cc192ef6bb6d94b02ca9",
+    "url": "/beneficiados-auxilio-emergencial/static/js/runtime-main.71c4d53d.js"
+  }
+]);
